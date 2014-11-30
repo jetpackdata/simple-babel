d05c60bfb2a7b537a49f7fe1a188a903630efec8
shankararul <shankar.vinay.arul@gmail.com>
2014-11-30 21:10:16 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,8 @@ import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-  SECRET_KEY='\xc4\x92n\x15g\xb0\xa7\x00\x00\xa1\xe1\x91C0\xbe\xf4\xfe\xb8\xaf\x81\x10b\xe3\xcf'
+  SECRET_KEY='set a secret key here'
   DEBUG = True
-  SQLALCHEMY_DATABASE_URI       = os.environ.get('env_db_uri')
   LANGUAGES = {
     'en': 'English',
     'fr': 'Français'
